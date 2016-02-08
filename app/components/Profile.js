11670b7743a2cc3dcce794c198973b828e77b5f6
“mgoel1” <mgoel@walmartlabs.com>
2016-02-07 19:51:02 -0800

    update components to support new feature

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,12 +1,16 @@
 
 var React = require('react');
 var Router = require('react-router');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
 
 var Profile = React.createClass({
+
+  mixins: [ReactFireMixin],
 
   getInitialState: function() {
 
@@ -21,6 +25,13 @@ var Profile = React.createClass({
 
   },
 
+  componentDidMount: function() {
+    // called when component gets mounted in the React DOM
+    this.ref = new Firebase('https://incandescent-torch-3919.firebaseio.com/');
+    var childRef = this.ref.child(this.props.params.username);
+    this.bindAsArray(childRef, 'notes'); // this.state.notes
+  },
+
   render: function() {
 
     return (
@@ -29,10 +40,10 @@ var Profile = React.createClass({
           <UserProfile username={this.props.params.username} bio={this.state.bio} />
         </div>
         <div className="col-md-4">
-          <Repos repos={this.state.repos} />
+          <Repos username={this.props.params.username} repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          <Notes notes={this.state.notes} />
+          <Notes username={this.props.params.username} notes={this.state.notes} />
         </div>
       </div>
     )
